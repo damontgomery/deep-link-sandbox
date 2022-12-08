dcd06e71947f3ba15979290b68815dcfefc98a0b
Dan Montgomery <dmontgomery@phase2technology.com>
2022-12-08 13:53:36 -0600

    Use HTTPS
    not sure if this matters.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/server-redirect', function(req, res) {
-  res.redirect('http://deep-link-sandbox-intercept.local/intercept.html');
+  res.redirect('https://deep-link-sandbox-intercept.local/intercept.html');
 });
 
 // catch 404 and forward to error handler
